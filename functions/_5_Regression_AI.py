e84e442356289d1df03adfbffe2075af76586af3
sergio-dv <Sergio.Dominguez@ciemat.es>
2024-10-30 12:04:31 +0100

    Evolution of 5_Regression_time_NN

--- functions/_5_Regression_AI.py
+++ functions/_5_Regression_AI.py
@@ -50,7 +50,7 @@ def split_train_test(image, hit_nuvT_filtered, test_ratio=0.50):
     return x_train, x_test, train_nuvT, test_nuvT
 
 
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
 
 def train_and_predict(model, x_train, train_nuvT, x_test, test_nuvT, epochs=30, batch_size=32):
     """
@@ -70,20 +70,22 @@ def train_and_predict(model, x_train, train_nuvT, x_test, test_nuvT, epochs=30, 
         nuvT_pred: Predictions on the test data.
     """
 
-    # Define the ModelCheckpoint callback
-    weights_file="/tmp/weights_nuvT.hdf5"
-    checkpoint = ModelCheckpoint(weights_file, monitor='mse', verbose=0, save_best_only=True, mode='min')
-    callbacks = [checkpoint]
+    # Define the callbacks
+    weights_file = "/tmp/weights_nuvT.hdf5.keras"
+    checkpoint = ModelCheckpoint(weights_file, monitor='val_loss', verbose=0, save_best_only=True, mode='min')
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+    callbacks = [checkpoint, early_stopping]
 
     # Train the model
     history = model.fit(
         x_train,
         train_nuvT,
         epochs=epochs,
-        batch_size=batch_size,  # Updated parameter name to 'batch_size'
+        batch_size=batch_size,
         callbacks=callbacks,
         validation_data=(x_test, test_nuvT),
-        verbose=2
+        verbose=1
     )
 
     # Load the best weights
