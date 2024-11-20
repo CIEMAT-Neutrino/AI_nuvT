4c6989743c38fb3e9df8f0ad7f8938124d4bc17a
sergio-dv <Sergio.Dominguez@ciemat.es>
2024-11-20 19:52:42 +0100

    new developments in 6, new validation loss comparison plots

--- functions/_5_Regression_AI.py
+++ functions/_5_Regression_AI.py
@@ -50,41 +50,50 @@ def split_train_test(image, hit_nuvT_filtered, test_ratio=0.50):
     return x_train, x_test, train_nuvT, test_nuvT
 
 
-from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping, ReduceLROnPlateau
 
-def train_and_predict(model, x_train, train_nuvT, x_test, test_nuvT, epochs=30, batch_size=32):
+def train_and_predict(model, x_train, y_train, x_test, y_test, epochs=30, batch_size=32):
     """
     Trains the model and predicts the output.
 
     Parameters:
         model: The Keras model to be trained.
         x_train: Training input data.
-        train_nuvT: Training output data.
+        y_train: Training output data.
         x_test: Testing input data.
-        test_nuvT: Testing output data.
+        y_test: Testing output data.
         epochs: Number of epochs for training.
         batch_size: Batch size for training.
-        weights_file: Path to save the best weights.
 
     Returns:
-        nuvT_pred: Predictions on the test data.
+        y_pred: Predictions on the test data.
+        history: Training history object.
     """
 
     # Define the callbacks
     weights_file = "/tmp/weights_nuvT.hdf5.keras"
-    checkpoint = ModelCheckpoint(weights_file, monitor='val_loss', verbose=0, save_best_only=True, mode='min')
-    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
-    callbacks = [checkpoint, early_stopping]
+    checkpoint = ModelCheckpoint(
+        weights_file, monitor='val_loss', verbose=0, save_best_only=True, mode='min'
+    )
+    early_stopping = EarlyStopping(
+        monitor='val_loss', patience=5, restore_best_weights=True
+    )
+    reduce_lr = ReduceLROnPlateau(
+        monitor='val_loss', factor=0.5, patience=3, min_lr=1e-6, verbose=1
+    )
+
+    # Add all callbacks to the list
+    callbacks = [checkpoint, early_stopping, reduce_lr]
 
     # Train the model
     history = model.fit(
         x_train,
-        train_nuvT,
+        y_train,
         epochs=epochs,
         batch_size=batch_size,
         callbacks=callbacks,
-        validation_data=(x_test, test_nuvT),
+        validation_data=(x_test, y_test),
         verbose=1
     )
 
@@ -92,6 +101,6 @@ def train_and_predict(model, x_train, train_nuvT, x_test, test_nuvT, epochs=30, 
     model.load_weights(weights_file)
 
     # Make predictions on the test set
-    nuvT_pred = model.predict(x_test)
+    y_pred = model.predict(x_test)
 
-    return nuvT_pred, history
+    return y_pred, history
